d9f206919ec38983aed9bbfadb15f9409cf551f5
alex.oreshkevich <alexander.oreshkevich@gmail.com>
2020-03-03 22:22:20 +0300

    Wildfly deployment using maven plugin

--- src/main/java/com/rednavis/showcase/HelloServlet.java
+++ src/main/java/com/rednavis/showcase/HelloServlet.java
@@ -23,7 +23,24 @@ public class HelloServlet extends HttpServlet {
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    System.out.println("Hello from servlet");
+    // obtain servlet context (may be empty)
+    String servletContext = request.getContextPath();
+    System.out.printf("[doGet] context path %s\n", servletContext);
+    String jndiBeanLookup = "java:global".concat(servletContext).concat("/CalculatorBean");
+
+    /* See Wildfly logs for JNDI binding details:
+    22:15:11,659 INFO  [org.jboss.as.ejb3.deployment] (MSC service thread 1-8) WFLYEJB0473:
+      JNDI bindings for session bean named 'CalculatorBean'
+       in deployment unit 'deployment "javaee-showcase.war"' are as follows:
+
+        java:global/javaee-showcase/CalculatorBean!com.rednavis.showcase.example.CalculatorBean
+        java:app/javaee-showcase/CalculatorBean!com.rednavis.showcase.example.CalculatorBean
+        java:module/CalculatorBean!com.rednavis.showcase.example.CalculatorBean
+        ejb:/javaee-showcase/CalculatorBean!com.rednavis.showcase.example.CalculatorBean
+        java:global/javaee-showcase/CalculatorBean
+        java:app/javaee-showcase/CalculatorBean
+        java:module/CalculatorBean
+     */
 
     CalculatorBean calculator = (CalculatorBean) request.getSession().getAttribute(CALCULATOR_SESSION_KEY);
     if (calculator == null) {
@@ -32,11 +49,9 @@ public class HelloServlet extends HttpServlet {
       // We obtain a CartBean instance and add it to the session object.
       try {
         InitialContext ic = new InitialContext();
-        calculator = (CalculatorBean) ic.lookup("java:global/CalculatorBean");
-
+        calculator = (CalculatorBean) ic.lookup(jndiBeanLookup);
         request.getSession().setAttribute(CALCULATOR_SESSION_KEY, calculator);
-
-        System.out.println("CalculatorBean created");
+        System.out.println("Lookup success. CalculatorBean created");
 
       } catch (NamingException e) {
         throw new ServletException(e);
